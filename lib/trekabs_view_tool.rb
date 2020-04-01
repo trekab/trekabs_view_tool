d4cc2a382854eb4fd92aaf892cb9b31ad145e649
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-01 05:00:23 +0300

    Trekabs tool initial commit

---
+++ lib/trekabs_view_tool.rb
@@ -0,0 +1,7 @@
+require "trekabs_view_tool/version"
+require "trekabs_view_tool/renderer"
+
+module TrekabsViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/trekabs_view_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TrekabsViewTool do
+  it "has a version number" do
+    expect(TrekabsViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
